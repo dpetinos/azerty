f2dded8090d7643f6b86f274c359d4f779c2ab2d
dimitri.petinos <dimitri.petinos@softbankrobotics.com>
2018-07-26 19:59:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Table1 from './app/components/Table1'
+ 
+var data = [
+  {id: 1, name: 'Gob', value: '2'},
+  {id: 2, name: 'Buster', value: '5'},
+  {id: 3, name: 'George Michael', value: '4'}
+];
+ 
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="Table-header">Basic Table</p>
+        <Table1 data={data}/>
+      </div>
+    );
+  }
+}
+ 
+export default App;
